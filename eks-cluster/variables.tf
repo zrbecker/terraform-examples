8cc347d55a498259f8746986a64b7615adaedcd3
Zachary Becker <zrbecker@gmail.com>
2025-06-21 21:51:46 -0400

    use version 1.32 for ARM compat

--- eks-cluster/variables.tf
+++ eks-cluster/variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version to use for the EKS cluster"
   type        = string
-  default     = "1.33"
+  default     = "1.32"
 }
 
 variable "vpc_cidr" {
